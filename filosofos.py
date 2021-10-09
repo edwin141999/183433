f8bc877036014147bd466a4756a8eba9fecd7e34
edwin141999 <183433@ids.upchiapas.edu.mx>
2021-10-09 17:34:43 -0500

    codigo finalizado

--- filosofos.py
+++ filosofos.py
@@ -2,28 +2,46 @@ import time
 import random
 import threading
 import queue
+import sys
 
-queue = queue.Queue(maxsize=5)
+platos = queue.Queue(maxsize=5)
 
-def Productor():
-    while True:
-        if not queue.full():
-            item = random.randint(1,5)
-            queue.put(item)
-            print('\nElemento de cola',item)
-            time.sleep(random.randint(1,5))
+filosofos = ['Socrates','Platon','Heraclito','Aristóteles','Tales de Mileto']
+contador = 0
+
+def Filosofos():
+    global contador
+    while contador <5:
+        if not platos.full():
+            filosofo = random.choice(filosofos)
+            filosofos.remove(filosofo)
+            platos.put(filosofo)
+            print('Filosofo:',filosofo,"esta comiendo")
+            contador+=1
+            time.sleep(random.randint(1,3))
             
-def Consumidor():
-    while True:
-        if not queue.empty():
-            item = queue.get()
-            queue.task_done()
-            print('\nElemento terminado',item)
-            time.sleep(random.randint(1,5))
+def Cena():
+    global contador
+    while contador <=5:
+        if not platos.empty():
+            cena = platos.get()
+            platos.task_done()
+            print('Filosofo:',cena, 'termino de comer\n')
+            time.sleep(random.randint(1,3))
             
 if __name__ == "__main__":
-    thread_productor = threading.Thread(target=Productor)
-    thread_consumidor = threading.Thread(target=Consumidor)
+    thread_productor = threading.Thread(target=Filosofos)
+    thread_consumidor = threading.Thread(target=Cena)
+    
+    thread_productor.daemon = True
+    thread_consumidor.daemon = True
     
     thread_productor.start()
     thread_consumidor.start()
+    
+    auxiliar=5
+    
+    while True:
+        if contador == auxiliar:
+            time.sleep(2)
+            sys.exit()
